a05cba1fe128292729057a5f278f9c3955be2b70
Suhail Abdelaal <suhail.abdelaal@gmail.com>
2025-03-21 00:54:00 +0200

    add AccessControl.sol

---
+++ src/AccessControl.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract RoleBasedAccessControl is AccessControl {
+
+
+}
