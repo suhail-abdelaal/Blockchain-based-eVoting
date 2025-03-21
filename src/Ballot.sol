d66f452b6aa38c16adc68b8859108476d4ca4e6e
Suhail Abdelaal <suhail.abdelaal@gmail.com>
2025-03-21 14:32:24 +0200

    rename file AccessControl to RoleBasedAccessControl

--- src/Ballot.sol
+++ src/Ballot.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {RoleBasedAccessControl} from "./AccessControl.sol";
+import {RoleBasedAccessControl} from "./RoleBasedAccessControl.sol";
 
 contract Ballot is RoleBasedAccessControl{
 
